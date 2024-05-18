9eea2ba92b38f4fe5886db64961224ba5bc0f34b
Emilly Nogueira Da Silva <157144329+EmillyNogueiraDaSilva@users.noreply.github.com>
2024-05-18 15:23:44 -0300

    Subindo o meu portfólio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*typing animation */
+var typed = new Typed(".typing", {
+    strings:["","Técnica em Informática para Internet","Web Designer","Web Developer","Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
